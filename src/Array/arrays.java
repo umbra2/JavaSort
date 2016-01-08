837cb5908b2bc6f8796d65a8640be768c049c0f1
Dmitriy <umbra2@mail.ru>
2016-01-09 00:07:32 +0300

    Добавлен алгоритм создания массива из случайных чисел и его сортировка

--- src/Array/arrays.java
+++ src/Array/arrays.java
@@ -1,7 +1,38 @@
 package Array;
 
-/**
- * Created by dima on 06.01.16.
- */
+import java.lang.reflect.*;
+
 public class arrays {
+    private int[] arr = new int[10];
+    public arrays(){
+        entArray(arr);
+    }
+
+    public static void entArray(int[] arr){
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = (int) (Math.random() * 100);
+        }
+    }
+
+    public static void print(Object array){
+        System.out.println(toString(array));
+    }
+    public static String toString(Object array){
+        if (array == null ||
+                !array.getClass().isArray()) {
+            return String.valueOf(array);
+        }
+        StringBuffer sb = new StringBuffer();
+        int len = Array.getLength(array);
+        sb.append('[');
+        for (int i = 0; i < len; i++){
+            if(i > 0){
+                sb.append(" , ");
+            }
+            Object obj = Array.get(array, i);
+            sb.append(toString(obj));
+        }
+        sb.append(']');
+        return sb.toString();
+    }
 }
--- src/Sort.java
+++ src/Sort.java
@@ -1,5 +1,16 @@
-/**
- * Created by dima on 06.01.16.
- */
+import java.util.Arrays;
+import Array.*;
 public class Sort {
+    public static void main(String[] args){
+        int[] arr1 = new int[10];
+
+        arrays.entArray(arr1);
+
+        arrays.print(arr1);
+
+        Arrays.sort(arr1);
+
+        arrays.print(arr1);
+
+    }
 }
